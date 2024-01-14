38431dcd0b9080a4edba6ef797a8b4b8591e1fc2
Awule Joshua <awulejoshua823@gmail.com>
2024-01-14 15:13:51 -0600

    Updated Store tables

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ from apps.catalogue.models import Product
 class Store(models.Model):
     location_choices =[('BW', 'Bweyale'), ('KY', 'Kyradongo')]
     name = models.CharField(max_length=15)
+    owner = models.ForeignKey(User, related_name="shop", on_delete=models.CASCADE)
     location = models.CharField(max_length=20, choices=location_choices)
     staff = models.ManyToManyField(User, related_name='stores')
     products = models.ManyToManyField(Product, related_name='store')
