06ac8b850ec9cc1c13da50c041729326baf368b9
Awule Joshua <awulejoshua823@gmail.com>
2024-01-16 01:00:48 -0600

    Settings page added

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name='store'
 urlpatterns = [
-    path('settings/', views.SettingsView.as_view(), name='settings')
+    path('settings/', views.SettingsView.as_view(), name='settings'),
+    path('change-profile-format/', views.ProfileImageView.as_view(), name='profile'),
+    path('update-profile/', views.UpdateProfileImageView.as_view(), name='update')
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http import JsonResponse
+from django.views.generic import TemplateView, View
 
 class SettingsView(TemplateView):
     template_name = 'store/settings.html'
+
+
+class ProfileImageView(View):
+    def post(self, request, *args, **kwargs):
+        upload_image = request.FILES.get('profile_image')
+
+        profile_image_url = '/media/profiles/' + str(upload_image)
+
+
+        return JsonResponse({'profile_image_url': profile_image_url})
+
+class UpdateProfileImageView(View):
+    def post(self, request, *args, **kwargs):
+        print(request.POST)
+        profile_image_url = request.POST.get('profile_image_url')
+
+        if profile_image_url:
+            request.user.profile = profile_image_url
+            request.user.save()
+        else:
+            print('Not found')
+            print(profile_image_url)
+        return JsonResponse({'message': 'Profile image updated successfully'})
+
+
 
