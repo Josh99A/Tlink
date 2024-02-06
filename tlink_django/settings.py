2b9324c2841e5ea86efaf96e8e43b45b4fb042f2
Awule Joshua <128831208+Josh99A@users.noreply.github.com>
2024-02-06 07:27:38 +0300

    Update settings.py

--- tlink_django/settings.py
+++ tlink_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") != 'False'
+DEBUG = os.getenv("DEBUG") 
 
 ALLOWED_HOSTS = ["localhost", "web-production-a191.up.railway.app"]
 
