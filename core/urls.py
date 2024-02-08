173b8a15baeb80890a1d499b9a502ea6050ddd0d
Awule Joshua <128831208+Josh99A@users.noreply.github.com>
2024-02-08 07:12:47 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
 
-from django.urls import reverse
 from . import views
 from apps.dashboard.catalogue.views import TlinkProductCreateUpdateView
 
@@ -12,7 +11,7 @@ urlpatterns = [
     path('subscribe/', views.Subscribe.as_view(), name='subscribe'),
     path('<str:subscription>/<str:payment_method>/signup', views.SignUp.as_view(), name='signup'),
     path('<int:pk>/store', views.UserStoreView.as_view(), name='store'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html',next_page=reverse('store:settings', kwargs={'pk':request.user.pk})),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html',),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/', )),
     re_path(
                 r"^products/create/(?P<product_class_slug>[\w-]+)/$",
