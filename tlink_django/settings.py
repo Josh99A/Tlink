da83e6d3ecc8cf764876aa1dd28b0e741ed5712f
Awule Joshua <awulejoshua823@gmail.com>
2024-02-16 12:38:42 +0300

    Including env and database

--- tlink_django/settings.py
+++ tlink_django/settings.py
@@ -159,34 +159,25 @@ WSGI_APPLICATION = 'tlink_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Tlink',
+#         'NAME': 'railway',
 #         'USER': "postgres",
-#         'PASSWORD':"awule" ,
-#         'HOST':"localhost",
-#         'PORT':"5432",
+#         'PASSWORD':"1eD1FDg4b*eGbcGdefa6F6DbF6GeBGEa" ,
+#         'HOST':"monorail.proxy.rlwy.net",
+#         'PORT':"43338",
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': "postgres",
-        'PASSWORD':"1eD1FDg4b*eGbcGdefa6F6DbF6GeBGEa" ,
-        'HOST':"monorail.proxy.rlwy.net",
-        'PORT':"43338",
-    }
-}
 
 
 
